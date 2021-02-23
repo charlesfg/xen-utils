c3ac94fa2e25d709adac4ee9a4ff6f6b02ec3125
Charles Goncalves <charles.fg@gmail.com>
2021-02-23 12:13:55 -0300

    Fixing typos and adding info in errors

--- attacks/vm2handler/vm2handler.py
+++ attacks/vm2handler/vm2handler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
 This script will receive:
     target - a script to run inside the ps (process selector)
@@ -47,13 +47,13 @@ if __name__ == '__main__':
         description='Script to get a physical page address (in the current context)'
                     'given a process selector script and a specification\n'
                     'Must be root to run this p')
-    parser.add_argument('--host', '-h', metavar='host', type=str, required=True,
+    parser.add_argument('--host', '-H', metavar='host', type=str, required=True,
                         help='Host where the script will get the pages.')
     parser.add_argument('--region', '-r', metavar='memory_regions', type=str, choices=('stack', 'heap', 'vdso', 'vvar'),
                         required=True, help='Area from possible addresses to obtain, one of: %(choices)s', )
     parser.add_argument('--action', '-a', metavar='action', type=str, choices=('write'),
                         required=True, help='Action to take in the page, one of: %(choices)s', )
-    parser.add_argument('--order', '-o', metavar='page-order', type=str, choices=('first', 'last', 'random'),
+    parser.add_argument('--order', '-o', metavar='page-order', required=True, type=str, choices=('first', 'last', 'random'),
                         default='first',
                         help='The order of the page to return, one of: %(choices)s')
     parser.add_argument('--hex', '-x', type=lambda x: hex(int(x, 0)), required=False,
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     # Validate all values
 
     # Start Script
-    script_logger = ScriptLogger(__file__, debug=DEBUG, mute_console=not args.quiet)
+    script_logger = ScriptLogger(__file__, debug=DEBUG, mute_console=args.quiet)
     logger = script_logger.get_main_logger()
 
     logger.debug("Starting vm2handler with: {}".format(args))
@@ -87,13 +87,14 @@ if __name__ == '__main__':
         exit(1)
 
     logger.info("Get GPFN from target host")
-    cmd = "ssh {} 'cd vm2hander && python3 domU_broker.py --script {} --area {} " \
-          "--order {}'".format(host["hostname"], args.script, args.area, args.order)
+    cmd = "ssh {} 'cd vm2handler && python3 domU_broker.py --script {} --region {} " \
+          "--order {}'".format(host["hostname"], args.script, args.region, args.order)
     logger.info("Will invoke the command:\n{}".format(cmd))
     try:
         gpfn = run_cmd(cmd)
     except subprocess.CalledProcessError as e:
         logger.error("Could not acquire the gpfn\n:{}".format(e))
+        logger.error("output:\n{}".format(e.output))
         logger.error("!! {}".format(traceback.format_exc()))
         exit(1)
 
