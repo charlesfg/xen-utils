3671fbab8b0269532fe22660d405ed1e8a86f81e
Charles Goncalves <charles.fg@gmail.com>
2024-05-31 14:36:39 +0000

    adding effector patch

--- xen-install/ubuntu/20.04/xen_4.18/update_grub.sh
+++ xen-install/ubuntu/20.04/xen_4.18/update_grub.sh
@@ -1,7 +1,5 @@
 echo 'GRUB_DISABLE_OS_PROBER=true
-GRUB_CMDLINE_XEN_DEFAULT="dom0_mem=4096M,max:4096M loglvl=all guest_loglvl=all \
-dom0_max_vcpus=2 com1=115200,8n1,0xd8c0,18 console=com1,vga sync_console \
-mce_verbosity=verbose mce=off"
+GRUB_CMDLINE_XEN_DEFAULT="dom0_mem=4096M,max:4096M loglvl=all guest_loglvl=all dom0_max_vcpus=2 com1=115200,8n1,0xd8c0,18 console=com1,vga sync_console mce_verbosity=verbose mce=off"
 GRUB_DEFAULT="Ubuntu GNU/Linux, with Xen hypervisor"
 '> /etc/default/grub.d/xen.cfg
 
