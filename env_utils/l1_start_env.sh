3b059f2053165436f50f429c93234b413b392601
Charles Goncalves <charles.fg@gmail.com>
2024-07-08 12:02:16 +0000

    checkpoint Mon Jul  8 12:02:16 UTC 2024

---
+++ env_utils/l1_start_env.sh
@@ -0,0 +1,107 @@
+#!/bin/bash -x
+
+# Function to check if Xen is installed
+check_xen() {
+    command -v xl &> /dev/null && sudo xl info &> /dev/null
+}
+
+# Function to check if KVM is installed
+check_kvm() {
+    command -v virsh &> /dev/null && sudo virsh --version &> /dev/null
+}
+
+check_vm_ssh_accessibility() {
+    local VM_IP="$1"
+    local SSH_USER="$2"
+
+    ssh -o ConnectTimeout=5 -o BatchMode=yes -o StrictHostKeyChecking=no ${SSH_USER}@${VM_IP} exit
+    return $?
+}
+
+check_if_xen_vm_running() {
+    local VM_NAME="$1"
+    sudo xl list | grep -q ${VM_NAME}
+    return $?
+}
+
+check_if_kvm_vm_running() {
+    local VM_NAME="$1"
+    sudo virsh list --all | grep ${VM_NAME} | grep -q running 
+    return $?
+}
+
+
+# Function to check all hosts in a round-robin fashion
+check_all_vms() {
+    local hosts=("$@")
+    local all_up=false
+
+    while [ $attempt -lt $OVERALL_MAX_ATTEMPTS ]; do
+        all_up=true
+
+        for VM_IP in "${hosts[@]}"; do
+            echo "Checking VM ${VM_IP} (attempt $(($attempt + 1)))..."
+            check_vm_ssh_accessibility "$VM_IP" "$SSH_USER"
+
+            if [ $? -ne 0 ]; then
+                echo "VM ${VM_IP} not ready yet."
+                all_up=false
+            else
+                echo "VM ${VM_IP} is up."
+            fi
+        done
+
+        if [ "$all_up" = true ]; then
+            echo "All VMs are up."
+            return 0
+        fi
+
+        echo "Not all VMs are up yet, retrying in $SLEEP_INTERVAL seconds..."
+        sleep $SLEEP_INTERVAL
+        ((attempt++))
+    done
+
+    echo "Not all VMs started up within the expected time."
+    return 1
+}
+
+all_up=true
+if check_xen; then
+    echo "Xen hypervisor detected."
+    if ! check_if_xen_vm_running sysbench; then
+        sudo xl create /etc/xen/sysbench.cfg
+        all_up=false
+    fi
+    if ! check_if_xen_vm_running compromised; then
+        sudo xl create /etc/xen/compromised.cfg
+        all_up=false
+    fi
+    sudo xl list
+else
+    echo "No supported hypervisor detected. Please install Xen or KVM."
+    exit 1
+fi
+
+if [ "$all_up" = true ]; then
+    echo "All VMs are up."
+    exit 0
+else
+    sleep 10
+fi
+
+# Call the function with the list of hosts
+# List of VMs and SSH user
+
+HOSTS=("172.16.0.13" "172.16.0.14")
+SSH_USER="xen"  
+OVERALL_MAX_ATTEMPTS=10  # Overall maximum number of attempts
+SLEEP_INTERVAL=5  # Seconds between each overall attempt
+
+# Tracking attempts
+attempt=0
+
+
+if ! check_all_vms "${HOSTS[@]}"; then
+    echo "Couldn't bring the hosts up!"
+fi
+
--- xen-install/ubuntu/20.04/xen_4.18/build.sh
+++ xen-install/ubuntu/20.04/xen_4.18/build.sh
@@ -1,10 +1,10 @@
-make clean -j
+make clean -j 
 
 ./configure \
 	--enable-systemd \
 	--enable-debug
 
-make debug=y -j	
+make debug=y -j	2
 
 
 sudo make install
